2387c4b6886c63bcf20d1c9c75324993d63ccad9
Sofia Berlin <sofiamberlin@gmail.com>
2018-05-13 22:35:59 -0700

    add pineapple layer

---
+++ assets/pineapple.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pineapple" tilewidth="70" tileheight="70" tilecount="1" columns="1">
+ <image source="pineapple.png" width="70" height="70"/>
+</tileset>
--- js/main.js
+++ js/main.js
@@ -31,6 +31,9 @@ function preload() {
   this.load.tilemapTiledJSON('map', 'assets/map.json');
   // tiles in spritesheet 
   this.load.image('tiles', 'assets/tiles.png')
+  // pineapple
+  // simple coin image
+  this.load.image('pineapple', 'assets/pineapple.png');
   // load player sprites
   this.load.spritesheet('player', 
     '/assets/player.png',
@@ -51,6 +54,11 @@ function create() {
   // the player will collide with this layer
   groundLayer.setCollisionByExclusion([-1]);
 
+  //pineapple layer
+  var pineappleTiles = map.addTilesetImage('pineapple');
+  // add coins as tiles
+  pineappleLayer = map.createDynamicLayer('Pineapples', pineappleTiles, 0, 0);
+
   //set the boundaries of our game world
   this.physics.world.bounds.width = groundLayer.width;
   this.physics.world.bounds.height = groundLayer.height;
@@ -63,6 +71,57 @@ function create() {
   player.setScale(0.9);
   // player will collide with the level tiles 
   this.physics.add.collider(groundLayer, player);
+
+  //player anims
+  this.anims.create ({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
+    framerate: 3,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'idle',
+    frames: [ { key: 'player', frame: 0 } ],
+    framerate: 2,
+  });
+
+  this.anims.create ({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', { start: 6, end: 8 }),
+    framerate: 2,
+    repeat: -1,
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  ////* Camera *////
+
+  // set bounds so the camera won't go outside the game world
+  this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+  // make the camera follow the player
+  this.cameras.main.startFollow(player);
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown)
+  {
+    player.body.setVelocityX(-200);
+    player.anims.play('left', true); // walk left
+    player.flipX = true; // flip the sprite to the left
+  }
+  else if (cursors.right.isDown)
+  {
+    player.body.setVelocityX(200);
+    player.anims.play('right', true);
+    player.flipX = false; // use the original sprite looking to the right
+  } else {
+    player.body.setVelocityX(0);
+    player.anims.play('idle', true);
+  }
+  // jump 
+  if (cursors.up.isDown && player.body.onFloor())
+  {
+    player.body.setVelocityY(-500);        
+  }
+}
