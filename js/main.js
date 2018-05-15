9a4bb1cb2ab73b9c537a7404b7b07ebf04056cb0
Sofia Berlin <sofiamberlin@gmail.com>
2018-05-14 20:56:25 -0700

    collide with trumps

--- js/main.js
+++ js/main.js
@@ -17,14 +17,18 @@ var config = {
   }
 };
 
-var game = new Phaser.Game(config);
+game = new Phaser.Game(config);
 
 var map;
 var player;
 var cursors;
 var groundLayer, pineappleLayer;
+var trumps;
 var text;
 var score = 0;
+var timer;
+
+
 
 function preload() {
   // map made with Tiled in JSON format
@@ -39,9 +43,15 @@ function preload() {
     '/assets/player.png',
     { frameWidth: 50, frameHeight: 85 }
   )
+  // load trump sprite
+  this.load.spritesheet('trump', 
+    '/assets/trump.png',
+    { frameWidth: 102, frameHeight: 70 }
+  )
 }
 
 function create() {
+
   /////* load the map *//////
 
   map = this.make.tilemap({key: 'map'});
@@ -53,16 +63,24 @@ function create() {
   this.cameras.main.setBackgroundColor('#87cefa'); 
   // the player will collide with this layer
   groundLayer.setCollisionByExclusion([-1]);
-
   //pineapple layer
   var pineappleTiles = map.addTilesetImage('pineapple');
-  // add coins as tiles
-  pineappleLayer = map.createDynamicLayer('Pineapples', pineappleTiles, 0, 0);
 
   //set the boundaries of our game world
   this.physics.world.bounds.width = groundLayer.width;
   this.physics.world.bounds.height = groundLayer.height;
 
+  /////* Score Text */////
+
+  // this text will show the score
+  text = this.add.text(20, 20, '0', {
+    fontSize: '60px',
+    fill: '#ffffff'
+  });
+  // fix the text to the camera
+  text.setScrollFactor(0);
+
+ 
   //////* Player */////
 
   player = this.physics.add.sprite(100, 200, 'player');
@@ -95,12 +113,76 @@ function create() {
 
   cursors = this.input.keyboard.createCursorKeys();
 
+  ///* Pineapples *///
+  pineappleLayer = map.createDynamicLayer('Pineapples', pineappleTiles, 0, 0);
+  pineappleLayer.setTileIndexCallback(18, collectPineapple, this);
+  this.physics.add.overlap(player, pineappleLayer);
+
+  function collectPineapple(sprite, tile) {
+    pineappleLayer.removeTileAt(tile.x, tile.y); // remove the tile/coin
+    score+=10; // add 10 points to the score
+    text.setText(score); // set the text to show the current score
+    return false;
+  }
+
   ////* Camera *////
 
   // set bounds so the camera won't go outside the game world
   this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   // make the camera follow the player
   this.cameras.main.startFollow(player);
+
+  /////* Trump */////
+
+  trumps = this.physics.add.group({
+    key: 'trump',
+    repeat: 3,
+    setXY: {
+      x: 400,
+      y: 100,
+      stepX: 400,
+      stepY: 0,
+    },
+  });
+  this.physics.add.collider(trumps, groundLayer);
+  this.physics.add.collider(player, trumps, hitTrump, null, this);
+  function hitTrump (player, bombs) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+  }
+
+  this.anims.create ({
+    key: 'trump-left',
+    frames: this.anims.generateFrameNumbers('trump', { start: 6, end: 11 }),
+    framerate: 5,
+    repeat: -1,
+  });
+
+  this.anims.create ({
+    key: 'trump-right',
+    frames: this.anims.generateFrameNumbers('trump', { start: 18, end: 23 }),
+    framerate: 5,
+    repeat: -1,
+  });
+
+  trumps.children.iterate(function(trump){
+    trump.collideWorldBounds = true;
+    trump.setBounce(.2);
+    if (Math.random() < .5) {
+      trump.setVelocityX(100);
+      trump.anims.play('trump-left');
+    } else {
+      trump.setVelocityX(-100)};
+      trump.anims.play('trump-right');
+  });
+
+  // timedEvent = this.time.addEvent({
+  //   delay: 2000,
+  //   callback: hitTrump,
+  //   callbackScope: this
+  // });
+
 }
 
 function update() {
